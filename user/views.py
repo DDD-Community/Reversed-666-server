7760bac403ad25fecd803602ad50c128193f0915
이혜연 <ihyeyeon@ihyeyeon-ui-MacBookAir.local>
2021-11-02 18:35:43 +0900

    feat: 🎸 임시 식별자 발급하고 user DB에 저장하는 로직 구현

--- user/views.py
+++ user/views.py
@@ -1,12 +1,11 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import authentication, permissions
+from rest_framework import authentication, permissions, serializers
 from django.shortcuts import render
 from rest_framework import status
 from drf_yasg.utils import swagger_auto_schema
-
-
-    
+from .models import User
+import uuid
 
 class AnonymousUserView(APIView):
     @swagger_auto_schema(tags=['유저 API'])
@@ -14,6 +13,9 @@ class AnonymousUserView(APIView):
         return Response(f"{anonymousId}에 해당하는 사용자의 정보를 가져옵니다.")
 
 class MakeAnonymousUserView(APIView):
+
     @swagger_auto_schema(tags=['유저 API'])
     def get(self, request):
-        return Response("새로운 임시 사용자를 생성합니다.")
+        Uuid = uuid.uuid4()
+        user = User.objects.create(anonymous_id = Uuid)
+        return Response("OK")
