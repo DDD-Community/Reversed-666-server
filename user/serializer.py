4bf24b7aebedeecd43f54389516e9095821cacb5
이혜연 <ihyeyeon@ihyeyeon-ui-MacBookAir.local>
2021-11-04 12:14:48 +0900

    refactor: 💡 serializer 코드 제외할 필드로 표현하도록 수정

--- user/serializer.py
+++ user/serializer.py
@@ -6,11 +6,10 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["id", "anonymous_id", "apple_id", "kakao_id", "email", "password", "nickname", "profile_image", "created_at", "updated_at"]
+        exclude = ("role", "Is_deleted", "deleted_at")
 
 class UserCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'anonymous_id'] 
 
-        
