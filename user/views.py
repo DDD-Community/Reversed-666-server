d20f42fc44236e5806310af245458a4a3175dc2a
이혜연 <ihyeyeon@ihyeyeon-ui-MacBookAir.local>
2021-11-04 12:09:57 +0900

    docs: ✏️ API respone type, model이 표시되도록 스웨거 설명 추가

--- user/views.py
+++ user/views.py
@@ -2,20 +2,20 @@ from django.http.response import JsonResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import authentication, permissions, serializers
-from django.shortcuts import render
-from rest_framework import status
+from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
 from .models import User
 from .serializer import UserSerializer, UserCreateSerializer
-from django.http import HttpResponse
-import json
 import uuid
+
+user_response = openapi.Response('응답 예시', UserSerializer)
+user_create_response = openapi.Response('응답 예시', UserCreateSerializer)
 
 class AnonymousUserView(APIView):
     '''
     사용자 Id를 받아 사용자의 정보를 돌려준다.
     '''
-    @swagger_auto_schema(tags=['유저 API'])
+    @swagger_auto_schema(tags=['유저 API'], responses = {200: user_response})
     def get(self, request, id):
         obj = User.objects.get(id=id)
         serializer = UserSerializer(obj)
@@ -26,7 +26,7 @@ class MakeAnonymousUserView(APIView):
     '''
     새로운 사용자를 생성하고, 사용자별로 고유한 임시 식별자를 발급한다.
     '''
-    @swagger_auto_schema(tags=['유저 API'])
+    @swagger_auto_schema(tags=['유저 API'], responses = {200: user_create_response})
     def get(self, request):
         anonymousId = uuid.uuid4()
         user = User.objects.create(anonymous_id = anonymousId)
