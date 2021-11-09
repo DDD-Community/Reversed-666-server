74e9678806aec10fef5a5174351211a9b536e7ad
HyeyeonLee <39080868+hyeyeon-sun@users.noreply.github.com>
2021-11-09 22:17:30 +0900

    Feat #23 API 전반적으로 리팩토링 (#24)
    * feat: 🎸 프론트 변경사항 반영(미반영사항 - 메인 브랜드에서 Is_liked 정보 추가 )
    
    * feat: 🎸 Is_liked 항목 추가
    
    * refactor: 💡 serializer 에서 Is_liked 처리하도록 리팩토링
    
    Co-authored-by: 이혜연 <ihyeyeon@ihyeyeon-ui-MacBookAir.local>

--- brand/serializer.py
+++ brand/serializer.py
@@ -9,12 +9,22 @@ from .models import mainBrand, Brand, likedBrand
 class BrandSerializer(serializers.ModelSerializer):
     class Meta:
         model = Brand
-        fields = ['id', 'name', 'en_name', 'site_url', 'logo_url']
+        fields = ['id', 'name', 'en_name', 'site_url', 'logo_url', 'click_count', 'like_count']
 
 class mainBranditemSerializer(serializers.ModelSerializer):
+    is_liked = serializers.SerializerMethodField()
+
+    def get_is_liked(self, obj):
+        try:
+            Is_liked = likedBrand.objects.get(user = self.context.get("user_id"), brand = obj.id)
+        except likedBrand.DoesNotExist:
+            Is_liked = False
+        else:
+            Is_liked = True
+        return Is_liked
     class Meta:
         model = Brand
-        fields = ['id', 'name', 'en_name', 'site_url', 'logo_url', 'Img_url']
+        fields = ['id', 'name', 'en_name', 'site_url', 'logo_url', 'Img_url', 'is_liked']
 
 class BrandIdNameSerializer(serializers.ModelSerializer):
     class Meta:
@@ -23,46 +33,23 @@ class BrandIdNameSerializer(serializers.ModelSerializer):
 
 #### main브랜드 관련 serializers ####
 
-# mainBrand 응답을 커스텀하기 위한 class
-class mainBrandType(object):
-    def __init__(self, list):
-        self.list = list
-
 # brands 테이블과 leftjoin된 brand_id 필드의 정보만 불러온다.
+
+class MainBrandType:
+    def __init__(self, brand, user):
+        self.brand = brand
+        self.user = user
+
+
 class brandJoinSerializer(serializers.ModelSerializer):
     brand = mainBranditemSerializer(read_only = True)
+    is_liked = serializers.SerializerMethodField()
 
+    def get_is_liked(self, obj):
+        return None
     class Meta:
         model = mainBrand
-        fields = ["brand"]
-
-# join된 정보에는 모두 brand_id column이 맨 앞에 붙어 있기에 제거해준다.
-class mainBrandSerializer(serializers.Serializer):
-    brandList = serializers.SerializerMethodField()
-
-    def get_brandList(self, obj):
-        data = list(map(lambda x : x['brand'], obj.list))
-        return data
-
-class swaggermainBrand(serializers.Serializer):
-    brandList = mainBranditemSerializer(many = True)
-    
-
-#### popular 브랜드 관련 serializers ####
-
-# popularBrand 응답을 커스텀하기 위한 class
-class popularBrandType(object):
-    def __init__(self, size, list):
-        self.size = size
-        self.sortBy = 'popular'
-        self.brandList = list
-
-# popularBrand를 객체로 받아 serialize 해준다.
-class popularBrandSerializer(serializers.Serializer):
-    size = serializers.IntegerField()
-    sortBy = serializers.CharField()
-    brandList = BrandSerializer(many = True)
-
+        fields = ["brand", 'is_liked']
 
 
 #### clickCount 관련 serializers ####
--- brand/urls.py
+++ brand/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path 
 from django.conf import settings 
-from .views import brandView, brandAddView, brandMainView,brandPopularView,brandSearchView, BrandCountView, markedBrandCountView, markedBrandSearchView, markedBrandView
+from .views import brandView, brandAddView, brandMainView,brandPopularView,brandSearchView, markedBrandCountView, markedBrandSearchView, markedBrandView
 
 urlpatterns = [ 
     path("brand/<int:brandId>", brandView.as_view(), name = "getBrand"),
@@ -8,8 +8,7 @@ urlpatterns = [
     path("brands/popular", brandPopularView.as_view(), name = "GetpopularBrands"),
     path("brands/main", brandMainView.as_view(), name = "GetmainBrands"),
     path("brands/search", brandSearchView.as_view({'get': 'list'}), name = "GetmainBrands"),
-    path("brands/marked", markedBrandView.as_view(), name = "GetmainBrands"),
-    path("brands/marked/<search>", markedBrandSearchView.as_view(), name = "GetmainBrands"),
-    path("brand/mark", markedBrandCountView.as_view(), name = "GetmainBrands"),
-    path("brand/count/<int:brandId>", BrandCountView.as_view(), name = "GetmainBrands"),
+    path("brands/liked", markedBrandView.as_view(), name = "GetmainBrands"),
+    path("brands/liked/<search>", markedBrandSearchView.as_view(), name = "GetmainBrands"),
+    path("brand/like", markedBrandCountView.as_view(), name = "GetmainBrands"),
 ]
